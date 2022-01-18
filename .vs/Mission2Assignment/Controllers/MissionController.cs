92c88fda8e0936b06cefcec66c2c6b8b8a4cfc0d
delangea <73494880+delangea@users.noreply.github.com>
2022-01-17 21:00:39 -0700

    all changes

---
+++ .vs/Mission2Assignment/Controllers/MissionController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission2Assignment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission2Assignment.Controllers
+{
+    public class MissionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult GradeCalculator()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult GradeCalculator(GradeModel model)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ .vs/Mission2Assignment/Models/GradeModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission2Assignment.Models
+{
+    public class GradeModel
+    {
+        [Range(0, 100)]
+        public int Assignments { get; set; }
+        [Range(0, 100)]
+        public int Projects { get; set; }
+        [Range(0, 100)]
+        public int Quizzes { get; set; }
+        [Range(0, 100)]
+        public int INTEX { get; set; }
+        [Range(0, 100)]
+        public int Exams { get; set; }
+    }
+}
